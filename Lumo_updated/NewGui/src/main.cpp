c8eb4d2b04d918eac721306b9bd4971490c2fe4e
julienvh <julienvanhaeren@live.nl>
2017-06-23 09:48:05 +0200

    Dit is de nieuwste versie van Lumo
    Bestaande uit: IK, facetranking, motoren aansturing, nieuwe GUI

---
+++ Lumo_updated/NewGui/src/main.cpp
@@ -0,0 +1,13 @@
+#include "ofMain.h"
+#include "ofApp.h"
+
+//========================================================================
+int main( ){
+	ofSetupOpenGL(1024,768,OF_WINDOW);			// <-------- setup the GL context
+	ofSetBackgroundColor(50, 50, 50, 255);
+	// this kicks off the running of my app
+	// can be OF_WINDOW or OF_FULLSCREEN
+	// pass in width and height too:
+	ofRunApp(new ofApp());
+
+}
